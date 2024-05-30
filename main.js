1abc43ef394dfaacd99129d9f8edc0f6b60158fd
octavio <octavio_arnica@hotmail.com>
2024-05-30 17:12:52 -0300

    avances

--- main.js
+++
@@ -1,6 +0,0 @@
-const dialog = document.querySelectorAll("dialog");
-const cancel = document.querySelector('.cancel');
-const show = document.querySelector('.show');
-
-show.addEventListener('click', () => dialog.showModal())
-cancel.addEventListener('click', () => dialog.close())
